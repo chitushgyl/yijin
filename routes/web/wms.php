b46deae9440faaa5c6111b4b0ce96186d1d2634f
chitushgyl <chitushgyl@163.com>
2022-12-19 11:45:06 +0800

    1

--- routes/web/wms.php
+++ routes/web/wms.php
@@ -172,6 +172,7 @@ Route::group([
     Route::any('/library/delLibraryOrder', 'LibraryController@delLibraryOrder');
     Route::any('/library/freeLibrarySku', 'LibraryController@freeLibrarySku');
     Route::any('/library/cancelGrounding', 'LibraryController@cancelGrounding');
+    Route::any('/library/getLibrarySige', 'LibraryController@getLibrarySige');
     Route::group([
         "middleware"=>['daily'],
     ], function(){
