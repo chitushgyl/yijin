087638c54662a001acd8465e8eb53f667c7da5b2
chitushgyl <chitushgyl@163.com>
2022-12-22 09:34:25 +0800

    1

--- app/Models/Tms/TmsMoney.php
+++ app/Models/Tms/TmsMoney.php
@@ -14,7 +14,7 @@ class TmsMoney extends Model{
     //protected $connection = 'connection-name';
 
     //指定数据库表 如果表名后带s则不需要 不带则需要
-    protected $table = 'tms_order';
+    protected $table = 'tms_money';
 
     //指定主键字段 默认为id
     //protected $primaryKey = 'id';
