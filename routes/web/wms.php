eae1f4c088cd364707ac895e84d068c67a6875db
chitushgyl <chitushgyl@163.com>
2022-12-22 11:01:50 +0800

    1

--- routes/web/wms.php
+++ routes/web/wms.php
@@ -280,7 +280,7 @@ Route::group([
     Route::any('/money/payeeCheck', 'MoneyController@payeeCheck');//收款方确认
     Route::any('/money/createMoney', 'MoneyController@createMoney');//收款方确认
     Route::any('/money/addMoney', 'MoneyController@addMoney');//收款方确认
-    Route::any('/money/addState', 'MoneyController@addState');//收款方确认
+    Route::any('/money/moneyState', 'MoneyController@moneyState');//收款方确认
     Route::group([
         "middleware"=>['daily'],
     ], function(){
