f4956eacd16a95ba4fecf902387696d2a951572c
chitushgyl <chitushgyl@163.com>
2023-02-14 11:04:51 +0800

    1

--- app/Models/Tms/TmsDiplasic.php
+++ app/Models/Tms/TmsDiplasic.php
@@ -14,7 +14,7 @@ class TmsDiplasic extends Model{
     //protected $connection = 'connection-name';
 
     //指定数据库表 如果表名后带s则不需要 不带则需要
-    protected $table = 'tms_city';
+    protected $table = 'tms_diplasic';
 
     //指定主键字段 默认为id
     //protected $primaryKey = 'id';
