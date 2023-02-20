7413e9e13e257df7e3e2e91b1ab2c5b0ba91552c
chitushgyl <chitushgyl@163.com>
2023-02-20 19:22:13 +0800

    1

--- app/Http/Admin/Company/AdministrateController.php
+++ app/Http/Admin/Company/AdministrateController.php
@@ -166,6 +166,12 @@ class AdministrateController extends CommonController{
                         $query->whereIn('id',$arr);
                         $query->select($selectMenu);
                         $query->orderBy('sort','asc');
+                        $query->with(['children' => function($query)use($selectMenu,$whereMenu2,$arr) {
+                            $query->where($whereMenu2);
+                            $query->whereIn('id',$arr);
+                            $query->select($selectMenu);
+                            $query->orderBy('sort','asc');
+                        }]);
                     }]);
                 }])->where($whereMenu)->whereIn('id',$arr)->select($selectMenu)->orderBy('sort','asc')->get()->toArray();
 
