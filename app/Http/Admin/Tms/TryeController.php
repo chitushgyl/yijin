ad242d0db5c7be0c3427c2131e160b154ee30793
chitushgyl <chitushgyl@163.com>
2023-03-27 14:42:18 +0800

    1

--- app/Http/Admin/Tms/TryeController.php
+++ app/Http/Admin/Tms/TryeController.php
@@ -1001,7 +1001,7 @@ class TryeController extends CommonController{
             case 'all':
 //                $data['total']=ErpShopGoodsSku::where($where)->count(); //总的数据量
                 $data['items']=TmsTryeCount::
-                    offset($firstrow)->limit($listrows)->orderBy('model')
+                    offset($firstrow)->limit($listrows)->groupBy('model')
                     ->get();
                 $data['group_show']='Y';
                 break;
@@ -1010,7 +1010,7 @@ class TryeController extends CommonController{
                 $where[]=['group_code','=',$group_info['group_code']];
 //                $data['total']=ErpShopGoodsSku::where($where)->count(); //总的数据量
                 $data['items']=TmsTryeCount::
-                offset($firstrow)->limit($listrows)->orderBy('model')
+                offset($firstrow)->limit($listrows)->groupBy('model')
                     ->get();
                 $data['group_show']='N';
                 break;
@@ -1018,7 +1018,7 @@ class TryeController extends CommonController{
             case 'more':
 //                $data['total']=ErpShopGoodsSku::where($where)->whereIn('group_code',$group_info['group_code'])->count(); //总的数据量
                 $data['items']=TmsTryeCount::whereIn('group_code',$group_info['group_code'])
-                ->offset($firstrow)->limit($listrows)->orderBy('model')
+                ->offset($firstrow)->limit($listrows)->groupBy('model')
                     ->get();
                 $data['group_show']='Y';
                 break;
