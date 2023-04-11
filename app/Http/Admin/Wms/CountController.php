4e51656d39ed19e901356cea1790e9f130329068
chitushgyl <chitushgyl@163.com>
2023-04-11 17:45:54 +0800

    1

--- app/Http/Admin/Wms/CountController.php
+++ app/Http/Admin/Wms/CountController.php
@@ -246,7 +246,7 @@ class CountController extends CommonController{
             $v->in_initial_count = WmsLibraryChange::where('inout_time','<',$start_time)->where('type','preentry')->where('sku_id',$v->self_id)->sum('change_num');
             $v->out_initial_count = WmsLibraryChange::where('inout_time','<',$start_time)->where('type','out')->where('sku_id',$v->self_id)->sum('change_num');
             $v->initial_count = $v->in_initial_count - $v->out_initial_count;
-            $v->jie_count = $v->in_count + $v->initial_count;
+            $v->jie_count = $v->in_count + $v->initial_count - $v->out_count;
 
             $v->good_describe =unit_do($v->wms_unit , $v->wms_target_unit, $v->wms_scale, $v->count);
             $v->button_info=$button_info;
