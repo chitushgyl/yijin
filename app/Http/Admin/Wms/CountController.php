27c3acd1434776a7f9606370333dd2fc6f2723c1
chitushgyl <chitushgyl@163.com>
2023-04-11 17:11:08 +0800

    1

--- app/Http/Admin/Wms/CountController.php
+++ app/Http/Admin/Wms/CountController.php
@@ -173,7 +173,7 @@ class CountController extends CommonController{
             ['type'=>'like','name'=>'warehouse_name','value'=>$warehouse_name],
             ['type'=>'=','name'=>'delete_flag','value'=>'Y'],
             ['type'=>'=','name'=>'use_flag','value'=>'Y'],
-            ['type'=>'>','name'=>'now_num','value'=>0],
+            ['type'=>'>=','name'=>'now_num','value'=>0],
             ['type'=>'>=','name'=>'inout_time','value'=>$start_time.' 00:00:00'],
             ['type'=>'<','name'=>'inout_time','value'=>$end_time.' 23:59:59'],
         ];
