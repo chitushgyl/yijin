981a100e36090199071ef6496e1e27960bd20b30
onlyonemap <m18623716061@163.com>
2023-04-14 18:12:16 +0800

    1

--- app/Http/Admin/Tms/TryeController.php
+++ app/Http/Admin/Tms/TryeController.php
@@ -910,6 +910,7 @@ class TryeController extends CommonController{
                         return $msg;
                     }
                 }catch(\Exception $e){
+                    dd($e);
                     DB::rollBack();
                     $msg['code'] = 302;
                     $msg['msg'] = "操作失败";
