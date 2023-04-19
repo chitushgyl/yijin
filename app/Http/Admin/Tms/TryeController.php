5acdc2a4c852bad2e5e518d0d14fc7ff3aa04652
onlyonemap <m18623716061@163.com>
2023-04-19 13:42:34 +0800

    1

--- app/Http/Admin/Tms/TryeController.php
+++ app/Http/Admin/Tms/TryeController.php
@@ -1711,8 +1711,8 @@ class TryeController extends CommonController{
             ['type'=>'=','name'=>'delete_flag','value'=>'Y'],
             ['type'=>'=','name'=>'use_flag','value'=>'Y'],
             ['type'=>'>=','name'=>'now_num','value'=>0],
-            ['type'=>'>','name'=>'date_time','value'=>$start_time],
-            ['type'=>'<=','name'=>'date_time','value'=>$end_time],
+            ['type'=>'>','name'=>'inout_time','value'=>$start_time],
+            ['type'=>'<=','name'=>'inout_time','value'=>$end_time],
         ];
 
         $where=get_list_where($search);
@@ -1724,7 +1724,7 @@ class TryeController extends CommonController{
             case 'all':
                 $data['total']=TmsTryeList::where($where)->count(); //总的数据量
                 $data['items']=TmsTryeList::with(['TmsTryeCount' => function($query)use($Signselect,$where1) {
-                   $query->where($where1);
+                 
                     $query->select($Signselect);
                 }])
                     ->with(['tryeOutList' => function($query)use($where) {
@@ -1747,7 +1747,7 @@ class TryeController extends CommonController{
                 $where[]=['group_code','=',$group_info['group_code']];
                 $data['total']=TmsTryeList::where($where)->count(); //总的数据量
                 $data['items']=TmsTryeList::with(['TmsTryeCount' => function($query)use($Signselect,$where1) {
-                    $query->where($where1);
+                 
                     $query->select($Signselect);
                 }])
                     ->with(['tryeOutList' => function($query)use($where) {
@@ -1769,7 +1769,7 @@ class TryeController extends CommonController{
             case 'more':
                 $data['total']=TmsTryeList::where($where)->whereIn('group_code',$group_info['group_code'])->count(); //总的数据量
                 $data['items']=TmsTryeList::with(['TmsTryeCount' => function($query)use($Signselect,$where1) {
-                    $query->where($where1);
+                   
                     $query->select($Signselect);
                 }])
                     ->with(['tryeOutList' => function($query)use($where) {
