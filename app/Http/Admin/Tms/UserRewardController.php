c0da7bc37963fa877b5f79fca098ea3eda7d1323
onlyonemap <m18623716061@163.com>
2023-04-20 22:09:19 +0800

    1

--- app/Http/Admin/Tms/UserRewardController.php
+++ app/Http/Admin/Tms/UserRewardController.php
@@ -825,8 +825,9 @@ class UserRewardController extends CommonController{
                     $list['file_id']            =$file_id;
                     $datalist[]=$list;
                     
-                    if (($fine_user == 'driver'&& ($v['company_fine'] || $v['company_fine']>0)) || ($fine_user == 'all' && ($v['company_fine'] || $v['company_fine']>0))){
-                        $user_award['self_id']            = generate_id('award_');
+                    if ($fine_user == 'driver'|| $fine_user == 'all'){
+                        if($v['company_fine'] || $v['company_fine']){
+                            $user_award['self_id']            = generate_id('award_');
                         $user_award['reward_id']          = $list['self_id'];
                         $user_award['user_id']            = $list['user_id'];
                         $user_award['user_name']          = $list['user_name'];
@@ -842,10 +843,13 @@ class UserRewardController extends CommonController{
                         $user_award['create_time']        = $user_award['update_time']=$now_time;
 
                         $user_award_list[]=$user_award;
+                        }
+                        
                         
                     }
-                    if (($fine_user == 'cargo'&& ($v['company_fine'] || $v['company_fine']>0)) || ($fine_user == 'all' && ($v['company_fine'] || $v['company_fine']>0))){
-                        $award['self_id']            = generate_id('award_');
+                    if ($fine_user == 'cargo' || $fine_user == 'all'){
+                        if($v['company_fine'] || $v['company_fine']>0){
+                           $award['self_id']            = generate_id('award_');
                         $award['reward_id']          = $list['self_id'];
                         $award['user_id']            = $list['escort'];
                         $award['user_name']          = $list['escort_name'];
@@ -860,7 +864,9 @@ class UserRewardController extends CommonController{
                         $award['create_user_name']   = $user_info->name;
                         $award['create_time']        = $award['update_time']=$now_time;
 
-                        $award_list[]=$award;
+                        $award_list[]=$award; 
+                        }
+                        
                         
                     }
 
