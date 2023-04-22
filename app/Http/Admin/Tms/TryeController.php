a2cd014dd32eca7cbcedc30eb2c12bff234bd29a
onlyonemap <m18623716061@163.com>
2023-04-22 10:32:00 +0800

    1

--- app/Http/Admin/Tms/TryeController.php
+++ app/Http/Admin/Tms/TryeController.php
@@ -1118,11 +1118,14 @@ class TryeController extends CommonController{
 
         $select = ['self_id','model','order_id','sale_price','initial_num','change_num','now_num','trye_num','use_flag','delete_flag','group_code','group_name','create_time'];
         $data['info']=TmsTryeCount::where($where)->where('now_num','>',0)->select($select)->get();
+        dump($data['info']);
+
         $arr = [];
         $res = [];
         foreach($data['info'] as $k => $v){
             $res = array_merge($arr,explode(',',$v->trye_num));
         }
+        dd($res);
         
         $msg['code']=200;
         $msg['msg']="数据拉取成功";
