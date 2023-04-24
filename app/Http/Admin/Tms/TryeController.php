a75345bae42714a787e394c6f6660fa113f9846d
onlyonemap <m18623716061@163.com>
2023-04-24 10:11:07 +0800

    1

--- app/Http/Admin/Tms/TryeController.php
+++ app/Http/Admin/Tms/TryeController.php
@@ -257,7 +257,7 @@ class TryeController extends CommonController{
             ['self_id','=',$self_id],
         ];
         $select=['self_id','car_number','price','car_number','model','supplier','num','trye_num','operator','type','in_time','driver_name','change','create_user_name','create_time','group_code','use_flag','state','user_id','trailer_num','remark','trye_name'];
-        $select1=['self_id','kilo','price','trye_img','change','order_id','model','num','trye_num','change','create_user_name','create_time','group_code','use_flag','trye_name'];
+        $select1=['self_id','kilo','price','trye_img','change','order_id','model','num','trye_num','change','create_user_name','create_time','group_code','use_flag','trye_name','sku_id','trye_sku_id'];
         $data['info']=TmsTrye::with(['TryeOutList'=>function($query)use($select1){
             $query->where('delete_flag','Y');
              $query->select($select1);
