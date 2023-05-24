61b2ca2488cac4bfdf6314a044966f98db091206
chitushgyl <chitushgyl@163.com>
2023-05-24 17:43:04 +0800

    修改库存统计排序

--- app/Http/Admin/Wms/CountController.php
+++ app/Http/Admin/Wms/CountController.php
@@ -90,8 +90,10 @@ class CountController extends CommonController{
 //                    $query->where('now_num','>','0');
                     $query->select($Signselect);
                 }])->where($where)
-                    ->offset($firstrow)->limit($listrows)->orderBy('external_sku_id','asc')
-                    ->select($select)->get();
+                    ->orderBy('external_sku_id','asc')
+                    ->offset($firstrow)->limit($listrows)
+                    ->select($select);
+//                    ->get();
                 $data['group_show']='Y';
                 break;
 
@@ -102,8 +104,10 @@ class CountController extends CommonController{
                     $query->where($where1);
                     $query->select($Signselect);
                 }])->where($where)
-                    ->offset($firstrow)->limit($listrows)->orderBy('external_sku_id','asc')
-                    ->select($select)->get();
+                    ->orderBy('external_sku_id','asc')
+                    ->offset($firstrow)->limit($listrows)
+                    ->select($select);
+//                    ->get();
                 $data['group_show']='N';
                 break;
 
@@ -113,9 +117,10 @@ class CountController extends CommonController{
                     $query->where($where1);
                     $query->select($Signselect);
                 }])->where($where)->whereIn('group_code',$group_info['group_code'])
-                    ->offset($firstrow)->limit($listrows)->orderBy('external_sku_id','asc')
-                    ->select($select)
-                    ->get();
+                    ->orderBy('external_sku_id','asc')
+                    ->offset($firstrow)->limit($listrows)
+                    ->select($select);
+//                    ->get();
                 $data['group_show']='Y';
                 break;
         }
